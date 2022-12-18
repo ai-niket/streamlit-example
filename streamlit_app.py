c6b69e0592c60c367e04824f879c87311c17139c
ai-niket <120767587+ai-niket@users.noreply.github.com>
2022-12-18 06:21:58 +0000

    train model based on uploaded file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 import sklearn.metrics as mat
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
 def test_and_print_metrics(y, y_pred, average, labels):
     """Print the accuracy, precision, recall, f1 score and confusion matrix of the model"""
@@ -17,5 +19,14 @@ def test_and_print_metrics(y, y_pred, average, labels):
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    dataframe
+    df = pd.read_csv(uploaded_file)
+    X = df.drop('Class', axis=1)
+    y = df['Class']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    knn = KNeighborsClassifier()
+    knn.fit(X_train, y_train)
+    y_test_pred = knn.predict(X_test)
+    test_and_print_metrics(y_test, y_test_pred, 'weighted', knn.classes_)
